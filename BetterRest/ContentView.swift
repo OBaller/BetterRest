d7d6b0d8570135a3a99dad238023637fcc7b845c
brightsummit.co <naseemoyebola@gmail.com>
2022-01-26 01:05:54 +0100

    UI Update

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,28 +5,81 @@
 //  Created by naseem on 07/01/2022.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State var stepperValue = 8
-    @State var wakeUpTime = Date.now
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = "Error"
+    @State private var alertMessage = "Sorry, there was a problem calculating your bedtime."
+    @State private var showingAlert = false
+    
+   static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View {
-        VStack {
-                Stepper("for \(stepperValue) regular sleep", value: $stepperValue, in: 3...14)
-                    .padding()
-                DatePicker("select time to wake up", selection: $wakeUpTime)
-//                .labelsHidden()
-                    .padding()
-            
-            Text(Date.now, format: .dateTime.hour().minute())
+        NavigationView {
+            Form {
+                VStack(alignment: .leading, spacing: 0){
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
+
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
-//    func exampleDate() {
-//        let tomorrow = Date.now.addingTimeInterval(86400)
-//        let range = Date.now...tomorrow
-//    }
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+                let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+
+        }
+        catch {
+            
+        }
+        showingAlert = true
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
